309432f95c5bcf809a06083688d17356def41580
Aaron Bedra <aaron@thinkrelevance.com>
2009-04-25 20:30:58 -0400

    first crack at project page

---
+++ javascripts/github-commit-badge.js
@@ -0,0 +1,19 @@
+;(function($) {
+		var user = "abedra";
+		var repo = "safe-erb";
+		var branch = "master"
+		var url = "http://github.com/api/v1/json/" + user + "/" + repo + "/commits/" + branch + '?callback=?';
+		$.getJSON(url, function(data) {
+				var commits = data.commits;
+
+				var commits_to_display = commits.length < 10 ? commits.length : 10;
+				for (var i = 0; i < commits_to_display; i++) {
+					var commit = commits[i];
+
+					var $commit_link = $('<a/>').attr('href', commit.url).text(commit.message);
+					var $message_li = $('<li/>').append($commit_link).append($commit_link);
+
+					$('.github-commits').append($message_li);
+				}
+			});
+	})(jQuery);
